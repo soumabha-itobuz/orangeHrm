fd534776ef53bc55caa8388a2a11b58bd4415fc0
Soumabha Roy <soumabha@itobuz.com>
2024-03-12 19:33:52 +0530

    fix: test execution fix

--- src/spec/vacancy-page.spec.ts
+++ src/spec/vacancy-page.spec.ts
@@ -2,18 +2,23 @@ import { test } from '@playwright/test';
 import { login } from '../lib/login';
 import { testConfig } from '../lib/test-config';
 
-test.describe('Add vacancy',  () => {
-  
-    test('login', async ({ page }) => {
-      await login(page, process.env.userName, process.env.password);
-    })
+test.describe('Add vacancy', () => {
+  let page;
 
-    test.use({ storageState: testConfig.authFile })
-    
-    test('Go to recruitment page ', async ({ page }) => {
-        await page.goto('/web/index.php/dashboard/index');
-        await page.click('ul  li:nth-child(5)  a  span');  
-        await page.click('nav > ul > li:nth-child(2) > a');
-        await page.click('div.orangehrm-header-container button');
-    });
-}) 
+  test.beforeEach(async ({ browser }) => {
+      page = await browser.newPage();
+      await page.goto(process.env.uiURl);
+      await login(page, process.env.userName, process.env.password);
+      await page.waitForURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+  });
+
+  test('Go to recruitment page ', async () => {
+      // Your test logic to navigate to the recruitment page
+      await page.click('ul  li:nth-child(5)  a  span');  
+      await page.click('nav > ul > li:nth-child(2) > a');
+      await page.click('div.orangehrm-header-container button');
+
+      // Add assertions or further actions
+  });
+
+});
