e391b8fc15f09ddb3bbcab6f8c755ea0765c200f
zeniaitobuz <zenia@itobuz.com>
2024-03-12 18:18:20 +0530

    feat: unsused import removed

--- src/spec/login-page.spec.ts
+++ src/spec/login-page.spec.ts
@@ -1,12 +1,10 @@
-import { test, expect, type Page } from '@playwright/test';
-import { login } from '../lib/login';
+import { test, type Page } from "@playwright/test";
+import { login } from "../lib/login";
 
-
-test.describe('Login page', () => {
-   
-    test('Log in to the application', async ({ page }) => {
-      await page.goto(process.env.uiURl);
-      await login(page, process.env.userName, process.env.password);
-      await page.locator('.oxd-text.oxd-text--h6').waitFor();
-    });
-}) 
+test.describe("Login page", () => {
+  test("Log in to the application", async ({ page }) => {
+    await page.goto(process.env.uiURl);
+    await login(page, process.env.userName, process.env.password);
+    await page.locator(".oxd-text.oxd-text--h6").waitFor();
+  });
+});
