65d901690cd5a0c01ee3ab8700e3004733b1f8ab
Soumabha Roy <soumabha@itobuz.com>
2024-03-13 02:54:35 +0530

    fix: save token

--- src/spec/vacancy-page.spec.ts
+++ src/spec/vacancy-page.spec.ts
@@ -1,27 +1,21 @@
 import { test } from '@playwright/test';
 import { login } from '../lib/login';
 
+let page;
+
 test.describe('Add vacancy', () => {
-  let page;
-
-  test.beforeEach(async ({ browser }) => {
-      page = await browser.newPage();
-      await page.goto(process.env.uiURl);
-      await login(page, process.env.userName, process.env.password);
-      await page.waitForURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+  test('login', async ({ browser }) => {
+    const token = ''; // Define token here or fetch it from somewhere
+    page = await browser.newPage();
+    await login(page, process.env.userName, process.env.password);
+    await page.evaluate(token => localStorage.setItem('Identity.States.token', token), token);
   });
 
-  test('Go to recruitment page ', async () => {
-      // Your test logic to navigate to the recruitment page
-      await page.click('ul  li:nth-child(5)  a  span');  
-      await page.click('nav > ul > li:nth-child(2) > a');
-      await page.click('div.orangehrm-header-container button');
-
-      // Add assertions or further actions
-  });
-
-  test.afterEach(async () => {
-      // Close the page after each test
-      await page.close();
+  test('Go to recruitment page', async () => {
+    // If the 'login' test is executed before this one, 'page' will contain the logged-in session
+    await page.goto('/web/index.php/dashboard/index');
+    await page.click('ul li:nth-child(5) a span');
+    await page.click('nav > ul > li:nth-child(2) > a');
+    await page.click('div.orangehrm-header-container button');
   });
 });
