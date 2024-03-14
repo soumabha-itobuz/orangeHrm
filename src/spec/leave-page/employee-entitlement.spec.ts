d3ef3237f35b974c788bae866b298599b359759d
zeniaitobuz <zenia@itobuz.com>
2024-03-14 19:06:56 +0530

    feat: employee entitlement wip

---
+++ src/spec/leave-page/employee-entitlement.spec.ts
@@ -0,0 +1,35 @@
+import { expect, test } from "@playwright/test";
+import { login } from "../../lib/login";
+
+test.describe("Leave / Employee Entitlement", () => {
+  let page;
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+  });
+
+  test.afterAll(() => {
+    page = null;
+  });
+
+  test("Log in to the application", async () => {
+    await login(page, process.env.userName, process.env.password);
+    await expect(page.locator(".oxd-topbar div div span h6")).toHaveText(
+      "Dashboard"
+    );
+  });
+
+  test("Add Leave Entitlement", async () => {
+    await page.click("ul  li:nth-child(3)  a  span");
+    await page.click("nav > ul > li:nth-child(3) > span ");
+    await page.click(".oxd-dropdown-menu > li:nth-child(2)");
+    await page
+      .locator(
+        ".oxd-form-row > div > div:nth-child(1) > div >  div:nth-child(2) > div > div > input"
+      )
+      .fill("Tiffany  Donovan");
+    await page.type(
+      ".oxd-form-row > div > div:nth-child(2) > div >  div:nth-child(2) > div > div > div:nth-child(1)",
+      "CAN - Bereavement"
+    );
+  });
+});
