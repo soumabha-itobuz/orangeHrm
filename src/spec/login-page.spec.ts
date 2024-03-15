5d8c5492230734173c17ba8047316e6e60c6a4af
Soumabha Roy <soumabha@itobuz.com>
2024-03-15 15:30:09 +0530

    fix: response

--- src/spec/login-page.spec.ts
+++ src/spec/login-page.spec.ts
@@ -1,11 +1,18 @@
 import { test } from '@playwright/test';
 import { login } from '../lib/login';
-
+// import * as fs from 'fs';
+// import * as path from 'path';
 
 test.describe("Login page", () => {
   test("Log in to the application", async ({ page }) => {
     await page.goto(process.env.uiURl);
     await login(page, process.env.userName, process.env.password);
     await page.locator(".oxd-text.oxd-text--h6").waitFor();
+
+    // const response = await page.waitForResponse(response => response.status() === 200);
+    // const responseBody = await response.text();
+    // //const filePath = 'src/spec/';
+    // path.resolve ('src/spec/');
+    // fs.writeFileSync(path.resolve ('src/spec/'), responseBody);
   });
 });
