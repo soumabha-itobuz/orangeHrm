3b1675747c2561a932927a310c3b1ab42c487870
zeniaitobuz <zenia@itobuz.com>
2024-03-21 16:59:42 +0530

    feat: color filter added

--- src/spec/evershop-filter/evershop-filter.spec.ts
+++ src/spec/evershop-filter/evershop-filter.spec.ts
@@ -1,11 +1,14 @@
 import { expect, test } from "@playwright/test";
-import path = require("path");
 
 const locators = {
   linkForMen: ".nav > li:nth-child(1)",
   linkForWomen: ".nav > li:nth-child(2)",
   priceSlider: '[type="range"]',
   priceListItems: ".sale-price",
+  colorTextOption:
+    ".product-filter-tool > div:nth-child(3) > ul > li > a > span",
+  productName: ".product-name > a",
+  productColor: ".specification > ul > li:nth-child(1) > span",
 };
 
 async function priceFilter(page, max: number, min: number) {
@@ -25,6 +28,30 @@ async function priceFilter(page, max: number, min: number) {
     const price = Number((await prices.nth(i).innerText()).substring(1));
     expect(price).toBeLessThanOrEqual(randomPrice);
     expect(price).toBeGreaterThanOrEqual(min);
+  }
+}
+
+async function colorFilterVerification(page) {
+  const colorFilter = await page.locator(locators.colorTextOption);
+  const count = await colorFilter.count();
+  for (let i = 0; i < count; i++) {
+    const color = await colorFilter.nth(i).innerText();
+    await colorFilter.nth(i).click();
+    await page.waitForTimeout(2000);
+    const product = await page.locator(locators.productName);
+    const countProduct = await product.count();
+    for (let j = 0; j < countProduct; j++) {
+      console.log(await product.nth(j).innerText());
+      await product.nth(j).click();
+      await page.waitForLoadState();
+      const productColorSpecification = await (
+        await page.locator(locators.productColor)
+      ).innerText();
+      expect(color).toBe(productColorSpecification);
+      await page.goBack();
+    }
+    await colorFilter.nth(i).click();
+    await page.waitForTimeout(2000);
   }
 }
 
@@ -48,11 +75,25 @@ test.describe("Filter for Evershop", () => {
     await page.waitForLoadState();
     priceFilter(page, 802, 169);
   });
+  test("Filter with color in the Men's section", async () => {
+    await page.goto("https://demo.evershop.io/");
+    await page.waitForLoadState();
+    await page.click(locators.linkForMen);
+    await page.waitForLoadState();
+    colorFilterVerification(page);
+  });
 
   test("Filter with price in the Women's section", async () => {
     await page.goto("https://demo.evershop.io/");
     await page.click(locators.linkForWomen);
     await page.waitForLoadState();
     priceFilter(page, 963, 133);
+  });
+  test("Filter with color in the Women's section", async () => {
+    await page.goto("https://demo.evershop.io/");
+    await page.waitForLoadState();
+    await page.click(locators.linkForWomen);
+    await page.waitForLoadState();
+    colorFilterVerification(page);
   });
 });
